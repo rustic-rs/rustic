63027055606746affa98f08cdd46c68e8da1b514
Alexander Weiss <alex@weissfam.de>
2022-02-04 08:17:23 +0100

    cat key doesn't need decryption

--- src/commands/cat.rs
+++ src/commands/cat.rs
@@ -1,26 +1,28 @@
-use anyhow::{bail, anyhow, Result};
+use anyhow::{anyhow, bail, Result};
 use clap::Parser;
 
 use crate::backend::{FileType, ReadBackend};
-use crate::index::{indexfiles::AllIndexFiles, ReadIndex};
 use crate::id::Id;
+use crate::index::{indexfiles::AllIndexFiles, ReadIndex};
 
 #[derive(Parser)]
 pub(super) struct Opts {
     /// file type to list
     tpe: String,
-    
+
     /// file type to list
     id: String,
 }
 
-pub(super) fn execute(be: &impl ReadBackend, opts: Opts) -> Result<()> {
+pub(super) fn execute(be: &impl ReadBackend, dbe: &impl ReadBackend, opts: Opts) -> Result<()> {
     let id = Id::from_hex(&opts.id)?;
 
     let tpe = match opts.tpe.as_str() {
         // special treatment for catingg blobs: read the index and use it to locate the blob
         "blob" => {
-            let blob = AllIndexFiles::new(be).get_id(id).ok_or(anyhow!("blob not found in index"))?;
+            let blob = AllIndexFiles::new(be)
+                .get_id(id)
+                .ok_or(anyhow!("blob not found in index"))?;
             let dec = be.read_partial(FileType::Pack, blob.pack, blob.bi.offset, blob.bi.length)?;
             print!("{}", String::from_utf8_lossy(&dec));
             return Ok(());
@@ -32,7 +34,12 @@ pub(super) fn execute(be: &impl ReadBackend, opts: Opts) -> Result<()> {
         t => bail!("invalid type: {}", t),
     };
 
-    let dec = be.read_full(tpe, id)?;
+    let dec = match tpe {
+        // special treatment for catting key files: those need no decryption
+        FileType::Key => be.read_full(tpe, id)?,
+        _ => dbe.read_full(tpe, id)?,
+    };
+
     print!("{}", String::from_utf8_lossy(&dec));
 
     Ok(())
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -4,8 +4,8 @@ use clap::{Parser, Subcommand};
 use crate::backend::{DecryptBackend, LocalBackend};
 use crate::repo;
 
-mod list;
 mod cat;
+mod list;
 
 #[derive(Parser)]
 #[clap(about, version)]
@@ -26,7 +26,7 @@ struct Opts {
 enum Command {
     /// list files
     List(list::Opts),
-    
+
     /// cat files
     Cat(cat::Opts),
 }
@@ -36,10 +36,10 @@ pub fn execute() -> Result<()> {
 
     let be = LocalBackend::new(&args.repository);
     let key = repo::find_key_in_backend(&be, &args.password, None)?;
-    let be = DecryptBackend::new(&be, key);
+    let dbe = DecryptBackend::new(&be, key);
 
     match args.command {
         Command::List(opts) => list::execute(&be, opts),
-        Command::Cat(opts) => cat::execute(&be, opts),
+        Command::Cat(opts) => cat::execute(&be, &dbe, opts),
     }
 }

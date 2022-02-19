582c6f174e6fcbd945bdba49b477368c4d0588d7
Alexander Weiss <alex@weissfam.de>
2022-02-19 09:06:59 +0100

    collect() into output table in snapshots command

--- src/commands/snapshots.rs
+++ src/commands/snapshots.rs
@@ -13,24 +13,26 @@ pub(super) fn execute(be: &impl ReadBackend, _opts: Opts) -> Result<()> {
     let mut snapshots = SnapshotFile::all_from_backend(be)?;
     snapshots.sort();
 
-    let mut table = Table::new();
+    let mut table: Table = snapshots
+        .into_iter()
+        .map(|sn| {
+            let paths = sn.paths.into_iter().map(|p| p + "\n").collect::<String>();
+            let time = sn.time.format("%Y-%m-%d %H:%M:%S");
+            let size = sn
+                .size
+                .map(|b| ByteSize(b).to_string_as(true))
+                .unwrap_or("?".to_string());
+            let files = sn
+                .file_count
+                .map(|c| c.to_string())
+                .unwrap_or("?".to_string());
+            row![sn.id, time, sn.hostname, "", paths, r->files, r->size]
+        })
+        .collect();
     table.set_titles(
         row![b->"ID", b->"Time", b->"Host", b->"Tags", b->"Paths", br->"Files", br->"Size"],
     );
     table.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
-    for sn in snapshots {
-        let paths = sn.paths.into_iter().map(|p| p + "\n").collect::<String>();
-        let time = sn.time.format("%Y-%m-%d %H:%M:%S");
-        let size = sn
-            .size
-            .map(|b| ByteSize(b).to_string_as(true))
-            .unwrap_or("?".to_string());
-        let files = sn
-            .file_count
-            .map(|c| c.to_string())
-            .unwrap_or("?".to_string());
-        table.add_row(row![sn.id, time, sn.hostname, "", paths, r->files, r->size]);
-    }
     table.printstd();
 
     Ok(())

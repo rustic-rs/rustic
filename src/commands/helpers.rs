cc40cd138f21b1f4b28f2e2459d782f05495f56f
Alexander Weiss <alex@weissfam.de>
2022-04-05 15:16:22 +0200

    Use indicatif 0.17-rc.10 (add ETA calculation)

--- src/commands/helpers.rs
+++ src/commands/helpers.rs
@@ -1,11 +1,11 @@
 use std::fs::File;
 use std::io::BufReader;
 use std::path::PathBuf;
-// use std::time::Duration; // with indicatif 0.17;
+use std::time::Duration;
 
 use anyhow::{bail, Result};
+use indicatif::HumanDuration;
 use indicatif::{ProgressBar, ProgressStyle};
-//use indicatif::HumanDuration; // with indicatif 0.17
 use rpassword::{prompt_password_stderr, read_password_with_reader};
 use vlog::*;
 
@@ -43,7 +43,8 @@ pub fn progress_counter() -> ProgressBar {
     if get_verbosity_level() == 1 {
         ProgressBar::new(0).with_style(
             ProgressStyle::default_bar()
-                .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>10}/{len:10}"),
+                .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>10}/{len:10}")
+                .unwrap(),
         )
     } else {
         ProgressBar::hidden()
@@ -54,16 +55,14 @@ pub fn progress_bytes() -> ProgressBar {
     if get_verbosity_level() == 1 {
         ProgressBar::new(0).with_style(
             ProgressStyle::default_bar()
-            /* with indicatif 0.17
             .with_key("my_eta", |s| 
                  match (s.pos(), s.len()){
                     (0, _) => "-".to_string(),
-                    (pos,len) => format!("{:#}", HumanDuration(Duration::from_secs(s.elapsed().as_secs() * (len-pos)/pos))),
+                    (pos,Some(len)) => format!("{:#}", HumanDuration(Duration::from_secs(s.elapsed().as_secs() * (len-pos)/pos))),
+                    (_, _) => "-".to_string(),
                 })
             .template("[{elapsed_precise}] {bar:40.cyan/blue} {bytes:>10}/{total_bytes:10} {bytes_per_sec:12} (ETA {my_eta})")
             .unwrap(),
-            */
-            .template("[{elapsed_precise}] {bar:40.cyan/blue} {bytes:>10}/{total_bytes:10} {bytes_per_sec:12}")
         )
     } else {
         ProgressBar::hidden()

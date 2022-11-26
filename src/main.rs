b60d28478bd041fb121904c8d810de62d17dea91
Alexander Weiss <alex@weissfam.de>
2022-11-26 08:17:17 +0100

    fix broken pipe panic

--- src/main.rs
+++ src/main.rs
@@ -41,5 +41,12 @@ mod index;
 mod repo;
 
 fn main() -> Result<()> {
+    // this is a workaround until unix_sigpipe (https://github.com/rust-lang/rust/issues/97889) is available.
+    // See also https://github.com/rust-lang/rust/issues/46016
+    #[allow(unsafe_code)]
+    unsafe {
+        libc::signal(libc::SIGPIPE, libc::SIG_DFL);
+    }
+
     commands::execute()
 }

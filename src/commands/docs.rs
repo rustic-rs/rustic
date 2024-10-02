4ab698fb393d1cd441affeaa677e1310aed9b5d3
simonsan <14062932+simonsan@users.noreply.github.com>
2024-10-02 02:18:57 +0200

    fix: use subcommands instead of flags
    Signed-off-by: simonsan <14062932+simonsan@users.noreply.github.com>

--- src/commands/docs.rs
+++ src/commands/docs.rs
@@ -2,33 +2,29 @@
 
 use abscissa_core::{status_err, Application, Command, Runnable, Shutdown};
 use anyhow::Result;
+use clap::Subcommand;
 
 use crate::{
     application::constants::{RUSTIC_CONFIG_DOCS_URL, RUSTIC_DEV_DOCS_URL, RUSTIC_DOCS_URL},
     RUSTIC_APP,
 };
 
+#[derive(Command, Debug, Clone, Copy, Default, Subcommand, Runnable)]
+enum DocsTypeSubcommand {
+    #[default]
+    /// Show the user documentation
+    User,
+    /// Show the development documentation
+    Dev,
+    /// Show the configuration documentation
+    Config,
+}
+
 /// Opens the documentation in the default browser.
-///
-/// # Note
-///
-/// If no flag is set, the user documentation will be opened.
-/// If the `dev` flag is set, the development documentation will be opened.
-/// If the `config` flag is set, the configuration documentation will be opened.
 #[derive(Clone, Command, Default, Debug, clap::Parser)]
-#[clap(group = clap::ArgGroup::new("documentation").multiple(false))]
 pub struct DocsCmd {
-    /// Open the user documentation
-    #[clap(short, long, group = "documentation")]
-    user: bool,
-
-    /// Open the development documentation
-    #[clap(short, long, group = "documentation")]
-    dev: bool,
-
-    /// Open the config documentation
-    #[clap(short, long, group = "documentation")]
-    config: bool,
+    #[clap(subcommand)]
+    cmd: Option<DocsTypeSubcommand>,
 }
 
 impl Runnable for DocsCmd {
@@ -42,21 +38,20 @@ impl Runnable for DocsCmd {
 
 impl DocsCmd {
     fn inner_run(&self) -> Result<()> {
-        let user_string = match (self.user, self.dev, self.config) {
-            // Default: If no flag is set, open the user documentation
-            (true, false, false) | (false, false, false) => {
+        let user_string = match self.cmd {
+            // Default to user docs if no subcommand is provided
+            Some(DocsTypeSubcommand::User) | None => {
                 open::that(RUSTIC_DOCS_URL)?;
                 format!("Opening the user documentation at {RUSTIC_DOCS_URL}")
             }
-            (false, true, false) => {
+            Some(DocsTypeSubcommand::Dev) => {
                 open::that(RUSTIC_DEV_DOCS_URL)?;
                 format!("Opening the development documentation at {RUSTIC_DEV_DOCS_URL}")
             }
-            (false, false, true) => {
+            Some(DocsTypeSubcommand::Config) => {
                 open::that(RUSTIC_CONFIG_DOCS_URL)?;
                 format!("Opening the configuration documentation at {RUSTIC_CONFIG_DOCS_URL}")
             }
-            _ => unreachable!("this should not be possible"),
         };
 
         println!("{user_string}");

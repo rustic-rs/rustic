dc9a44adb4829abea65711cd63c3f01f78d0eea0
Somethink1729 <max.gruetzner@libelle13.de>
2025-09-05 23:31:14 +0200

    add warning if only using --dry-run-warmup

--- src/commands/prune.rs
+++ src/commands/prune.rs
@@ -43,7 +43,10 @@ impl PruneCmd {
         let dry_run = config.global.dry_run;
         if dry_run && config.global.dry_run_warmup {
             repo.warm_up(prune_plan.repack_packs().into_iter())?;
-        } else if !dry_run && !config.global.dry_run_warmup {
+        } else if !config.global.dry_run_warmup {
+            info!("Ignoring --dry-run-warmup works only in combination with --dry-run");
+        }
+        if !dry_run {
             repo.prune(&self.opts, prune_plan)?;
         }
 

rustic-rs/rustic

commit d382d74241ffde690b75c64d99b7f2d789ad35cf

feat: Add environment variable substitution in config files (#1577)
closes #942 

This PR brings support for environment variable substitution in config
files. This simply uses the [subst](https://github.com/fizyr/subst)
crate to parse all environment variables before parsing the toml, this
allows to define toml values in environment variables for more
flexibility.

src/config.rs
@@ -13,7 +13,7 @@ use std::{
     path::PathBuf,
 };
 
-use abscissa_core::{FrameworkError, config::Config, path::AbsPathBuf};
+use abscissa_core::{FrameworkError, FrameworkErrorKind, config::Config, path::AbsPathBuf};
 use anyhow::{Result, anyhow};
 use clap::{Parser, ValueHint};
 use conflate::Merge;
@@ -120,7 +120,17 @@ impl RusticConfig {
 
         if let Some(path) = paths.iter().find(|path| path.exists()) {
             merge_logs.push((Level::Info, format!("using config {}", path.display())));
-            let mut config = Self::load_toml_file(AbsPathBuf::canonicalize(path)?)?;
+            let config_content = subst::substitute(
+                &std::fs::read_to_string(AbsPathBuf::canonicalize(path)?)?,
+                &subst::Env,
+            )
+            .map_err(|e| {
+                abscissa_core::error::context::Context::new(
+                    FrameworkErrorKind::ParseError,
+                    Some(Box::new(e)),
+                )
+            })?;
+            let mut config = Self::load_toml(config_content)?;
             // if "use_profile" is defined in config file, merge the referenced profiles first
             for profile in &config.global.use_profiles.clone() {
                 config.merge_profile(profile, merge_logs, Level::Warn)?;

Cargo.toml
@@ -139,6 +139,7 @@ opentelemetry = { version = "0.30.0", default-features = false, features = ["met
 opentelemetry-otlp = { version = "0.30.0", features = ["metrics"], optional = true }
 opentelemetry_sdk = { version = "0.30.0", default-features = false, features = ["metrics"], optional = true }
 rhai = { version = "1", features = ["sync", "serde", "no_optimize", "no_module", "no_custom_syntax", "only_i64"], optional = true }
+subst = "0.3.8"
 
 [dev-dependencies]
 abscissa_core = { version = "0.8.2", default-features = false, features = ["testing"] }

commit f0ddb47da45eb78621204bdf67b2c549a3d8fe5a

fix: Substitute environment variables in profiles only if explicitly opted-in (#1600)
Hi!

This PR fix the regression introduced in #1577 by adding an opt-in flag
for environment variable substitution in profile files. Users can still
use `$` in their file if they enable substitution by escaping them
(`\$`).

This PR closes #1598 and #942

src/config.rs
@@ -120,16 +120,17 @@ impl RusticConfig {
 
         if let Some(path) = paths.iter().find(|path| path.exists()) {
             merge_logs.push((Level::Info, format!("using config {}", path.display())));
-            let config_content = subst::substitute(
-                &std::fs::read_to_string(AbsPathBuf::canonicalize(path)?)?,
-                &subst::Env,
-            )
-            .map_err(|e| {
-                abscissa_core::error::context::Context::new(
-                    FrameworkErrorKind::ParseError,
-                    Some(Box::new(e)),
-                )
-            })?;
+            let config_content = std::fs::read_to_string(AbsPathBuf::canonicalize(path)?)?;
+            let config_content = if self.global.profile_substitute_env {
+                subst::substitute(&config_content, &subst::Env).map_err(|e| {
+                    abscissa_core::error::context::Context::new(
+                        FrameworkErrorKind::ParseError,
+                        Some(Box::new(e)),
+                    )
+                })?
+            } else {
+                config_content
+            };
             let mut config = Self::load_toml(config_content)?;
             // if "use_profile" is defined in config file, merge the referenced profiles first
             for profile in &config.global.use_profiles.clone() {
@@ -157,6 +158,11 @@ impl RusticConfig {
 #[derive(Default, Debug, Parser, Clone, Deserialize, Serialize, Merge)]
 #[serde(default, rename_all = "kebab-case", deny_unknown_fields)]
 pub struct GlobalOptions {
+    /// Substitute environment variables in profiles
+    #[clap(long, env = "RUSTIC_PROFILE_SUBSTITUTE_ENV")]
+    #[merge(strategy=conflate::bool::overwrite_false)]
+    pub profile_substitute_env: bool,
+
     /// Config profile to use. This parses the file `<PROFILE>.toml` in the config directory.
     /// [default: "rustic"]
     #[clap(

src/snapshots/rustic_rs__config__tests__default_config_display_passes.snap
@@ -3,6 +3,7 @@ source: src/config.rs
 expression: config
 ---
 [global]
+profile-substitute-env = false
 use-profiles = []
 dry-run = false
 check-index = false

src/snapshots/rustic_rs__config__tests__default_config_passes.snap
@@ -4,6 +4,7 @@ expression: config
 ---
 RusticConfig {
     global: GlobalOptions {
+        profile_substitute_env: false,
         use_profiles: [],
         group_by: None,
         dry_run: false,

src/snapshots/rustic_rs__config__tests__global_env_roundtrip_passes-2.snap
@@ -3,6 +3,7 @@ source: src/config.rs
 expression: deserialized
 ---
 [global]
+profile-substitute-env = false
 use-profiles = []
 dry-run = false
 check-index = false

src/snapshots/rustic_rs__config__tests__global_env_roundtrip_passes-3.snap
@@ -4,6 +4,7 @@ expression: deserialized
 ---
 RusticConfig {
     global: GlobalOptions {
+        profile_substitute_env: false,
         use_profiles: [],
         group_by: None,
         dry_run: false,

src/snapshots/rustic_rs__config__tests__global_env_roundtrip_passes.snap
@@ -3,6 +3,7 @@ source: src/config.rs
 expression: serialized
 ---
 [global]
+profile-substitute-env = false
 use-profiles = []
 dry-run = false
 check-index = false

tests/snapshots/show_config__show_config_passes.snap
@@ -3,6 +3,7 @@ source: tests/show-config.rs
 expression: output
 ---
 [global]
+profile-substitute-env = false
 use-profiles = []
 dry-run = false
 check-index = false
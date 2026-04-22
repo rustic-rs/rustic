cdeac63a0f99881d59876c45c41fb33623aaddf5
Turmaxx <88465473+Turmaxx@users.noreply.github.com>
2026-04-22 09:55:07 +0300

    add version command

--- src/commands.rs
+++ src/commands.rs
@@ -31,6 +31,7 @@ pub(crate) mod tag;
 pub(crate) mod tui;
 #[cfg(feature = "webdav")]
 pub(crate) mod webdav;
+pub(crate) mod version;
 
 use std::fmt::Debug;
 use std::path::PathBuf;
@@ -155,6 +156,9 @@ enum RusticCmd {
     /// Start a webdav server which allows to access the repository
     #[cfg(feature = "webdav")]
     Webdav(Box<WebDavCmd>),
+
+    /// Print version information
+    Version(Box<version::VersionCmd>),
 }
 
 fn styles() -> Styles {
@@ -265,21 +269,24 @@ impl Configurable<RusticConfig> for EntryPoint {
             }
         }
 
-        // start logger
-        config
-            .global
-            .logging_options
-            .start_logger(config.global.dry_run)
-            .map_err(|e| FrameworkErrorKind::ConfigError.context(e))?;
+        // start logger also check if version command was supplied by the user
+        // if so skip logging for verison
+        if !matches!(self.commands, RusticCmd::Version(_)) {
+            config
+                .global
+                .logging_options
+                .start_logger(config.global.dry_run)
+                .map_err(|e| FrameworkErrorKind::ConfigError.context(e))?;
 
-        if config.global.logging_options.log_file.is_some() {
-            info!("rustic {}", version());
-            info!("command: {:?}", std::env::args_os().collect::<Vec<_>>());
-        }
+            if config.global.logging_options.log_file.is_some() {
+                info!("rustic {}", version());
+                info!("command: {:?}", std::env::args_os().collect::<Vec<_>>());
+            }
 
-        // display logs from merging
-        for (level, merge_log) in merge_logs {
-            log!(level, "{merge_log}");
+            // display logs from merging
+            for (level, merge_log) in merge_logs {
+                log!(level, "{merge_log}");
+            }
         }
 
         match &self.commands {

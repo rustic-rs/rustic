7e451040534c3715ddcca7a81b0e830487d3542c
aawsome <37850842+aawsome@users.noreply.github.com>
2026-07-27 12:47:58 +0200

    fix: read stdin-filename from config file (#1813)

--- src/commands/backup.rs
+++ src/commands/backup.rs
@@ -67,10 +67,10 @@ pub struct BackupCmd {
     #[merge(skip)]
     name: Option<String>,
 
-    /// Set filename to be used when backing up from stdin
-    #[clap(long, value_name = "FILENAME", default_value = "stdin", value_hint = ValueHint::FilePath)]
-    #[merge(skip)]
-    stdin_filename: String,
+    /// Set filename to be used when backing up from stdin (default: "stdin")
+    #[clap(long, value_name = "FILENAME", value_hint = ValueHint::FilePath)]
+    #[merge(strategy=conflate::option::overwrite_none)]
+    stdin_filename: Option<String>,
 
     /// Start the given command and use its output as stdin
     #[clap(long, value_name = "COMMAND")]
@@ -444,7 +444,7 @@ impl BackupCmd {
         parent_opts.group_by = parent_opts.group_by.or(config.global.group_by);
 
         let backup_opts = BackupOptions::default()
-            .stdin_filename(self.stdin_filename)
+            .stdin_filename(self.stdin_filename.unwrap_or_else(|| "stdin".to_string()))
             .stdin_command(self.stdin_command)
             .as_path(self.as_path)
             .parent_opts(parent_opts)
